b984f3a25cf9539f42d5da754b1d90f7ea5d3c69
ChathuraD1392 <94224254+ChathuraD1392@users.noreply.github.com>
2025-02-21 09:20:10 +0530

    Add files via upload
    js added

---
+++ JS/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  MY_FACEBOOK: "https://www.facebook.com/chathura.dilshan.583",
+  MY_LINKEDIN: "httpS://www.linkedin.com/in/chathura-dilshan-rupasinghe",
+  MY_SERVICE: "service_xmuoy1d",
+  MY_TEMPLATE: "template_8oy4aw8",
+  MY_KEY: "ucqShmVgrdm9FdUah",
+  MY_SRC: "https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js",
+  MY_BLOG: "https://chathurasimplecodes.blogspot.com",
+};
---
+++ JS/mail.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent form from reloading the page
+
+      // Get input values
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+      var mobile = document.getElementById("mobile").value;
+      var subject = document.getElementById("subject").value;
+      var message = document.getElementById("message").value;
+
+      // Send email using EmailJS
+      emailjs
+        .send("service_xmuoy1d", "template_8oy4aw8", {
+          name: name,
+          email: email,
+          mobile: mobile,
+          subject: subject,
+          message: message,
+        })
+        .then(
+          function (response) {
+            alert("Message Sent Successfully!");
+            document.getElementById("contact-form").reset();
+          },
+          function (error) {
+            alert("Failed to send message. Please try again.");
+            console.log(error);
+          }
+        );
+    });
+});
